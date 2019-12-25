5744d8b2c5f40fae11d4cc938335f6dc8c85b67e
kumaomao <z1083271136@163.com>
2019-12-25 16:22:11 +0800

    tbk

---
+++ top/domain/Result.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * 返回结果
+ * @author auto create
+ */
+class Result
+{
+	
+	/** 
+	 * model
+	 **/
+	public $model;
+	
+	/** 
+	 * msgCode
+	 **/
+	public $msg_code;
+	
+	/** 
+	 * msgInfo
+	 **/
+	public $msg_info;
+	
+	/** 
+	 * 是否成功
+	 **/
+	public $success;	
+}
+?>
